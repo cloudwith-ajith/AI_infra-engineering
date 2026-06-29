f574b4d2a95fe221222534fe55ef2b88c413efa0
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-06-29 20:45:35 +0530

    Update NumpyPractice.py

--- NumpyPractice.py
+++ NumpyPractice.py
@@ -23,3 +23,13 @@ import numpy as np
 data = np.array([1, 2])
 ones = np.ones(2, dtype=np.int_)
 print(data + ones)
+
+import numpy as np
+b = np.array([[1, 1], [2, 2]])
+print(b.shape)
+//revserse the array
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+reverse =  np.flip(arr)
+print(reverse)
+
