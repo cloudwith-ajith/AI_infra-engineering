8c5d04680ed164346e6335e4a1dc816d58ed2083
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-06-30 21:57:42 +0530

    Update NumpyPractice.py

--- NumpyPractice.py
+++ NumpyPractice.py
@@ -90,4 +90,29 @@ print(a.max(),"max values")
 print(a.sum()," sum  of the array ")
 # copy the value
 x = a.copy()
+//pandas
 
+import pandas as pd
+import numpy as np
+
+# create the dataframe
+a = np.array(np.arange(1,11).reshape(2,5))
+df= pd.DataFrame(data = a,index = ["rows","r2"],columns =["c1","c2","c3","c4","c5"])
+# print(df)
+
+
+
+b = np.array(np.arange(1,11))
+df= pd.DataFrame(data = b.reshape(1,10),index = ["rows"],columns =["c1","c2","c3","c4","c5","c6","c7","c8","c9","c10"])
+# print(df)
+# showing the first five rows of the dataset 
+# print(df.head())
+
+
+m= pd.DataFrame(np.array(np.arange(1,21).reshape(4,5)))
+print(m)
+
+
+print(m.loc[2]) # this is used to get the row
+
+print(m.iloc[:2,:])  # this is one used to access the both the rows and columns 
