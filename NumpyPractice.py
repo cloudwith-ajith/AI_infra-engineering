1080533ce895afdd291b8936c378fd98c51db3c2
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-08-13 23:05:39 +0530

    Update NumpyPractice.py

--- NumpyPractice.py
+++ NumpyPractice.py
@@ -10,7 +10,7 @@ print(np.sort(arr))
  
 import numpy as np
 a = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
-
+  
 print(a)
 print(a[a < 5])
 #You can also select, for example, numbers that are equal to or greater than 5, and use that condition to index an array.
